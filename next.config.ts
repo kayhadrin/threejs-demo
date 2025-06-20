781793148f20f99e6e5f00edd02248ae317cb8b1
David Han Sze Chuen <182644+kayhadrin@users.noreply.github.com>
2025-06-20 03:23:59 +0000

    Enabling activing file polling in next.js turbopack watch causes bugs.
    See log errors when running `pnpm next dev --turbopack`
    
    More info: https://github.com/vercel/next.js/issues/80665#issuecomment-2989641829

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  watchOptions: {
+    pollIntervalMs: 1000, // some sensible number
+  },
 };
 
 export default nextConfig;
