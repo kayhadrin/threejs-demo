5f955e913984ed33181257e07cbf745ee7fa55b0
David Han Sze Chuen <182644+kayhadrin@users.noreply.github.com>
2025-06-22 18:28:10 +0000

    Create main data model TS interfaces

--- src/TypeUtils.tsx
+++ src/TypeUtils.tsx
@@ -5,6 +5,16 @@ import * as THREE from 'three';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 type ExtractDreiForwardRefTarget<T> = T extends ForwardRefComponent<infer P, infer T> ? T : never;
 
+// opaque type
+export type ID<Name extends string> = string & { _name: Name };
+
+export function castToID<Name extends string>(id: string): ID<Name> {
+  invariant(typeof id === 'string', 'ID must be a string');
+  return id as ID<Name>;
+}
+
+export type Nullish = null | undefined;
+
 export function isMeshNode(object?: THREE.Object3D): object is THREE.Mesh {
   return object?.type === 'Mesh';
 }
---
+++ src/models/Models.ts
@@ -0,0 +1,81 @@
+import { ID, Nullish } from '@/TypeUtils';
+
+/**
+ * Represents a material product
+ */
+export interface Product {
+  id: ID<'Product'>;
+  name: string;
+  containerTemplateID: ID<'ContainerTemplate'>;
+  /**
+   * Unit (mm)
+   */
+  customHeight?: number | Nullish;
+  /**
+   * Unit (mm)
+   */
+  customWidth?: number | Nullish;
+  /**
+   * Unit (mm)
+   */
+  customDepth?: number | Nullish;
+
+  // TODO
+  // labelImageID?: ID<'ImageAsset'>;
+}
+
+/**
+ * Represents a template for a product container.
+ */
+export interface ContainerTemplate {
+  id: ID<'ContainerTemplate'>;
+  name: string;
+  containerMaterialID: ID<'ContainerMaterial'>;
+  /**
+   * Unit (mm)
+   */
+  width: number;
+  /**
+   * Unit (mm)
+   */
+  height: number;
+  /**
+   * Unit (mm)
+   */
+  depth: number;
+  modelAssetID: ID<'ModelAsset'>;
+  thumbnailImageID: ID<'ImageAsset'>;
+}
+
+/**
+ * Represents the main material used for the container in the product.
+ */
+export interface ContainerMaterial {
+  id: ID<'ContainerMaterial'>;
+  name: string;
+}
+
+export type ImageAssetType = 'thumnail' | 'label';
+
+/**
+ * Represents an image asset that can be used in the application.
+ * It can be a thumbnail or a label image.
+ */
+export interface ImageAsset {
+  id: ID<'ImageAsset'>;
+  type: ImageAssetType;
+  width: number;
+  height: number;
+  desc: string;
+  thumbnailImageAssetID?: ID<'ImageAsset'> | Nullish;
+}
+
+/**
+ * Represents a 3D model asset that can be used in the application.
+ */
+export interface ModelAsset {
+  id: ID<'ModelAsset'>;
+  name: string;
+  desc: string;
+  licenseMd: string;
+}
