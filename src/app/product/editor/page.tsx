778ab1543e49f8101e12587d04ea073486ee2eaa
David Han Sze Chuen <182644+kayhadrin@users.noreply.github.com>
2025-06-22 18:27:21 +0000

    Tweak layout

--- src/app/product/editor/page.tsx
+++ src/app/product/editor/page.tsx
@@ -9,14 +9,14 @@ const ProductEditorCanvas = lazy(() => import('@/components/ProductEditorCanvas'
 
 export default function ProductEditorPage() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+      <main className="row-start-2 flex flex-col gap-[32px] sm:items-start">
         <Suspense fallback={null}>
           <ProductEditorCanvas />
         </Suspense>
         <h1 className="z-0 text-3xl font-bold tracking-tight sm:text-5xl">Product Editor</h1>
       </main>
-      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-[24px]"></footer>
+      <footer className="row-start-3 flex flex-wrap gap-[24px]"></footer>
     </div>
   );
 }
