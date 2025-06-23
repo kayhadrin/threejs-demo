bea655659b8243762ad4d05c9856482e2f590113
David Han Sze Chuen <182644+kayhadrin@users.noreply.github.com>
2025-06-23 20:51:18 +0000

    Remove unused file

--- src/components/ExtractRef.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-
-import { ForwardRefComponent } from '@react-three/drei/helpers/ts-utils';
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export type ExtractRef<T> = T extends ForwardRefComponent<infer P, infer T> ? T : never;
